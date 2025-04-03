f45b37c8177bbdc439af354d5da6caed6b347244
Omid Firouzbakht <134402130+omidfrou2013@users.noreply.github.com>
2025-04-03 10:08:32 +0200

    Update wdmatch.c

--- EX_02/Level_02/wdmatch/wdmatch.c
+++ EX_02/Level_02/wdmatch/wdmatch.c
@@ -10,37 +10,22 @@ void	ft_putstr(char *str)
 	}
 }
 
-int		ft_strlen(char *str)
-{
-	int i = 0;
-	while (str[i] != '\0')
-		i++;
-	return (i);
-}
-
 int		main(int ac, char **av)
 {
 	int i;
 	int j;
-	int wdlen;
 
 	if (ac == 3)
 	{
 		i = 0;
 		j = 0;
-		wdlen = 0;
-
-		while (av[2][j] != '\0' && av[1][i] != '\0')
+		while (av[1][i] != '\0' && av[2][j] != '\0')
 		{
 			if (av[1][i] == av[2][j])
-			{
-				wdlen++;
 				i++;
-			}
 			j++;
 		}
-
-		if (wdlen == ft_strlen(av[1]))
+		if (av[1][i] == '\0')
 			ft_putstr(av[1]);
 	}
 	write(1, "\n", 1);
